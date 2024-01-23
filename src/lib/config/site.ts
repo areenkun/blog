b1c26347a5a21fbf512add4cded0a382befd96d7
areenkun <101259369+areenkun@users.noreply.github.com>
2024-01-24 06:45:10 +0700

    Add files via upload

---
+++ src/lib/config/site.ts
@@ -0,0 +1,17 @@
+import type { SiteConfig } from '$lib/types/site'
+
+export const site: SiteConfig = {
+  protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'blog.areen.is-a.dev',
+  title: 'Areen',
+  subtitle: 'Codeforces is love, codeforces is life (•̀ᴗ•́ )و',
+  lang: 'en-US',
+  description: 'Passionate lifelong learner always looking to build new projects and expand my skills and a follower of current events who stays up to date on tech.',
+  author: {
+    avatar: '/assets/avatar.webp',
+    name: 'Areen el-Haq',
+    status: '🦍',
+    bio: 'Arch geek and ex-fansubber<br> with a passion for Linux, logic, languages, and shell scripting (•̀ᴗ•́ )و'
+  },
+  themeColor: '#3D4451'
+}

63e85c5b1c231d96baa9db8d727603a5bcf9cd00
areen-c <74430814+areen-c@users.noreply.github.com>
2024-01-24 07:11:11 +0700

    Update post.ts

--- src/lib/config/post.ts
+++ src/lib/config/post.ts
@@ -1,3 +1,19 @@
 import type { PostConfig } from '$lib/types/post'
 
-export const post: PostConfig = {}
+export const post: PostConfig = {
+    comment: {
+        use: ['Giscus'],
+        style: 'boxed', // comment system bar styles: none / bordered / lifted / boxed
+        giscus: {
+          repo: 'areenkun/blog',
+          repoID: 'R_kgDOLI0H3w',
+          category: 'Comments',
+          categoryID: 'DIC_kwDOIyERSs4CTt5a',
+          reactionsEnabled: true, // reactions: true / false
+          inputPosition: 'top', // position of comment box: top / bottom
+          lang: 'en', // language
+          theme: 'preferred_color_scheme', // theme
+          loading: 'lazy',
+        }
+      }    
+}

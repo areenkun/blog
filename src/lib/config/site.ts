0a1d8098e4c24182845f8db3376bdfb4f2cca81d
areen-c <74430814+areen-c@users.noreply.github.com>
2024-01-24 15:59:09 +0700

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,8 +2,8 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'blog.areen.is-a.dev',
-  title: 'Areen',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'areencx.pages.dev',
+  title: 'areen-c',
   subtitle: 'Codeforces is love, codeforces is life (•̀ᴗ•́ )و',
   lang: 'en-US',
   description: 'Passionate lifelong learner always looking to build new projects and expand my skills and a follower of current events who stays up to date on tech.',
